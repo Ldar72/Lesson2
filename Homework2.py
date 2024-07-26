11577cd237dec2e5bad647261041d207b8cc8647
Ldar72 <l-dar@mail.ru>
2024-07-26 14:45:29 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+amount_of_homeworks = 12
+amount_of_hours = 1.5
+title = 'Python'
+time_for_homework = (amount_of_hours) / (amount_of_homeworks)
+print ("Курс: " + str(title) + ", всего задач:", str(amount_of_homeworks) + ", среднее время выполнения:",  time_for_homework)
